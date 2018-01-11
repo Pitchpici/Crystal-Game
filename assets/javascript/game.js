4ce97e869bd338d4199c5fc822f8aae6d25df248
Roxana Milea <roxanamilea@Roxanas-MacBook-Air.local>
2018-01-10 23:54:16 -0500

    extra

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
-var num, j1, j2, j3, j4;
-var wins=0;
+var num, crystal1, crystal2, crystal3, crystal4;
+
+var wins = 0;
 $("#wins").html("<h4>" + wins + "</h4>");
-var losses=0;
+var losses = 0;
 $("#losses").html("<h4>" + losses + "</h4>");
-var playerScore=0;
+var playerScore = 0;
 $("#score").html("<h4>" + playerScore + "</h4>");
 
 function start() {
@@ -14,46 +15,56 @@ function start() {
 	//give value attribute the number was generated
 
 
-	num=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	num = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 	$("#number").html("<h4>" + num + "</h4>");
-	console.log(num);
-	j1=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-	$("#j1").attr("value", j1);
-	j2=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-	$("#j2").attr("value", j2);
-	j3=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-	$("#j3").attr("value", j3);
-	j4=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-	$("#j4").attr("value", j4);
-	console.log(j1, j2, j3, j4);
+
+	crystal1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	$("#c1").attr("value", crystal1);
+
+	crystal2=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	$("#c2").attr("value", crystal2);
+
+	crystal3=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	$("#c3").attr("value", crystal3);
+
+	crystal4=Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	$("#c4").attr("value", crystal4);
+
 };
 
+
+//when player clicks a button
 $(".btn").on("click", function(){
 
- 	var internal=$(this);
- 	var val=internal.attr("value");
+ 	var internal = $(this);
 
- 	playerScore+=parseInt(val);
- 	$("#score").html(playerScore);
+ 	//take a variable and save the value of the value attribute in it (it will be a string)
+ 	var val = internal.attr("value");
+
+ 	//transform that string into a integer (we know it's a integer because we generated it with math.random())
+ 	//and add it 
+ 	playerScore += parseInt(val);
+ 	$("#score").html("<h4>"+ playerScore+ "</h4>");
 	
-	if (num===playerScore) {
+
+	if (num === playerScore) {
 		
 		wins++;
 		$("#wins").html("<h4>" + wins + "</h4>"); 
 		
-		playerScore=0;
-		$("#score").html(playerScore);
+		playerScore = 0;
+		$("#score").html("<h4>"+ playerScore+ "</h4>");
 		
 		start();
 
 	}
-	else if (num<playerScore) {
+	else if (num < playerScore) {
 		
 		losses++;
 		$("#losses").html("<h4>" + losses + "</h4>");
 		
-		playerScore=0;
-		$("#score").html(playerScore);
+		playerScore = 0;
+		$("#score").html("<h4>"+ playerScore+ "</h4>");
 		
 		start();
 	}
